ed2f8da245cdb2f428cee09935b530a7c0fbed57
JackBidwell <jackbidwell2002@gmail.com>
2024-06-17 13:13:27 -0500

    Problem 1 solved find the sum of all the multiples of 3 and 5 under a certain interger

---
+++ Fibanacci.py
@@ -0,0 +1,10 @@
+num = 1
+fibanacci = []
+
+max = input("How many charecters of Fibanicci would you like calculate? " )
+max = int(max)
+
+i = 0
+while i < max:
+  fibanacci.append(fibanacci[i] + )
+
